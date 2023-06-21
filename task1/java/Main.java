7ea1db0182fab1d78e15ce32960b4f5ab2c67d94
asfdhk <134499499+asfdhk@users.noreply.github.com>
2023-06-21 20:43:15 +0300

    task 1
     task1

--- task1/java/Main.java
+++ task1/java/Main.java
@@ -1,10 +1,14 @@
 import static java.lang.Thread.sleep;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         for (int i = 0;i<100;i++){
             Demo ads =new Demo();
-            sleep(10);
+            try {
+                sleep(10);
+            }catch (InterruptedException e){
+                System.out.println("ERROR");
+            }
         }
     }
 }
